687347236a9ff0227f53ca8215add413a6c186a1
Deiver Kiel Retana <deikiel@gmail.com>
2023-01-17 23:44:33 -0600

    Update simple-assume-role.py

--- python/simple-assume-role.py
+++ python/simple-assume-role.py
@@ -1,24 +1,40 @@
 import boto3
 import configparser
 
-# Read credentials file
-config = configparser.ConfigParser()
-config.read(os.path.expanduser('~/.aws/credentials'))
+def authenticate_root_account(mfa_code):
+    # Read credentials file
+    config = configparser.ConfigParser()
+    config.read(os.path.expanduser("~/.aws/credentials"))
 
-# Get access key and secret key from credentials file
-access_key = config.get('root', 'aws_access_key_id')
-secret_key = config.get('root', 'aws_secret_access_key')
+    # Get access key and secret key
+    access_key = config.get("default", "aws_access_key_id")
+    secret_key = config.get("default", "aws_secret_access_key")
 
-# Authenticate with MFA
-mfa_code = input("Enter MFA code: ")
-session = boto3.Session(
-    aws_access_key_id=access_key,
-    aws_secret_access_key=secret_key,
-    mfa_serial_number='arn:aws:iam::ACCOUNT_ID:mfa/USERNAME',
-    mfa_token=mfa_code
-)
+    # Create STS client
+    client = boto3.client("sts", aws_access_key_id=access_key, aws_secret_access_key=secret_key)
 
-# List S3 buckets
-s3 = session.client('s3')
-buckets = s3.list_buckets()
-print(buckets)
+    # Get MFA session credentials
+    response = client.get_session_token(DurationSeconds=3600, SerialNumber=mfa_code)
+
+    # Return MFA session credentials
+    return response["Credentials"]
+
+def list_s3_buckets():
+    # Get MFA session credentials
+    mfa_credentials = authenticate_root_account(input("Enter MFA code: "))
+
+    # Create S3 client with MFA session credentials
+    s3 = boto3.client("s3", aws_access_key_id=mfa_credentials["AccessKeyId"],
+                      aws_secret_access_key=mfa_credentials["SecretAccessKey"],
+                      aws_session_token=mfa_credentials["SessionToken"])
+
+    # List S3 buckets
+    response = s3.list_buckets()
+    buckets = response["Buckets"]
+
+    # Print S3 bucket names
+    for bucket in buckets:
+        print(bucket["Name"])
+
+if __name__ == "__main__":
+    list_s3_buckets()
